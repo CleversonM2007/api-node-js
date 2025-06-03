3d466c7b4bac900db7f293ed4ebd483fd2a9c049
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-03 10:28:12 -0300

    mudanças
    mudanças

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,8 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
 
-const AutoresController = require('../controllers/livrosAutores');
-const GenerosController = require('../controllers/livrosGeneros');  
+const RotasCleverson= require ('./routes-cleverson')
 
-router.get('/livrosAutores', AutoresController.listarLivroAutores); 
-router.post('/livrosAutores', AutoresController.cadastrarLivroAutores); 
-router.patch('/livrosAutores/:aut_id', AutoresController.editarLivroAutores); //params
-router.delete('/LivrosAutores/:aut_id', AutoresController.apagarLivroAutores); //params
-
-router.get('/livrosGeneros', GenerosController.listarLivroGeneros); 
-router.post('/livrosGeneros', GenerosController.cadastrarLivroGeneros); 
-router.patch('/livrosGeneros/:gen_id', GenerosController.editarLivroGeneros); //params
-router.delete('/LivrosGeneros/:gen_id', GenerosController.apagarLivroGeneros); //params
+router.use('/',RotasCleverson);
 
 module.exports = router;
